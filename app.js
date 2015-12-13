4af2e50e3064a00cfe9b6841c974c82d0df73ea8
paras <rockypro92@gmail.com>
2015-12-13 10:44:47 +0530

    added remove function

--- app.js
+++ app.js
@@ -1,7 +1,116 @@
-var myTodo = angular.module('myTodo' ,[])
+var myTodo = angular.module('myTodo', []);
   angular.module('myTodo') 
-      .controller( 'TodoCtrl' , function(){
-         
-        var tasks = [];   
-      });
- 
+  .controller( 'TodoCtrl', function(){
+      
+      var todoList = this;
+      
+      todoList.initializeNewTodo = function(){
+        todoList.newTodo = {};
+      }
+      
+      todoList.addTodo = function() {
+          todoList.todo.push(todoList.newTodo);
+          
+          todoList.initializeNewTodo();
+      };
+      
+      todoList.removeTodo  = function(start) {
+          todoList.todo.splice(start,1);
+      };
+      
+      todoList.todo = [];
+      todoList.initializeNewTodo();
+      
+  });
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  
